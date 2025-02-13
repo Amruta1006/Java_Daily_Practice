2473603f1f3c89defe10f5151b152f6ca3727f91
Amruta <amrutabmaskar@gmail.com>
2025-02-13 22:55:22 +0530

    escape sequences

---
+++ src/Normal_Codes/escape_sequences.java
@@ -0,0 +1,14 @@
+package Normal_Codes;
+
+public class escape_sequences {
+    public static void main(String[] args) {
+        //use of /b
+        System.out.println("Hello\bEveyone");
+
+        //use of \"
+        System.out.println("Here \"we\" are");
+
+        //use of \t
+        System.out.println("Amruta \there");
+    }
+}

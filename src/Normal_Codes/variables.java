e486f100e4f9d8bef8d95292c0e28b568e26cf6d
Amruta <amrutabmaskar@gmail.com>
2025-02-13 22:18:27 +0530

    basic code in java

---
+++ src/Normal_Codes/variables.java
@@ -0,0 +1,25 @@
+package Normal_Codes;
+
+public class variables {
+    public static void main (String[] args){
+        //integer variable
+        int num = 1006;
+        System.out.println(num);
+
+        //float
+        float number = 10f;
+        System.out.println(number);
+
+        //double
+        double yournumber = 10.0603;
+        System.out.println(yournumber);
+
+        //string
+        String name = "Amruta";
+        System.out.println(name);
+
+        //boolean
+        boolean isAvailable = true;
+        System.out.println(isAvailable);
+    }
+}

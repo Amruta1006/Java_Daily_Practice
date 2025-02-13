a49d6f3f79f339267aac142013f5cb8206ffa48f
Amruta <amrutabmaskar@gmail.com>
2025-02-13 23:10:06 +0530

    patterns with escape sequences

---
+++ src/patterns/pattern_with_escape_sequences.java
@@ -0,0 +1,26 @@
+package patterns;
+
+public class pattern_with_escape_sequences {
+    public static void main(String[] args) {
+        //*
+        //* *
+        //* * *
+        //* * * *
+        //* * * * *
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
+
+        //* * * * *
+        //* * * *
+        //* * *
+        //* *
+        //*
+        System.out.println("* * * * *\n* * * *\n* * *\n* *\n*");
+
+        //        *
+        //      * *
+        //    * * *
+        //  * * * *
+        //* * * * *
+        System.out.println("         *\n       * *\n     * * *\n   * * * *\n * * * * *");
+    }
+}
